286b79af6e2683daaa416bc5b3aeae6425b4459a
m-winter <maciekwinter@wp.pl>
2021-08-10 17:42:17 +0200

    Add RecyclerView and problems are fixed

--- app/src/main/java/com/bawp/todoister/MainActivity.java
+++ app/src/main/java/com/bawp/todoister/MainActivity.java
@@ -2,18 +2,34 @@ package com.bawp.todoister;
 
 import android.os.Bundle;
 
+import com.bawp.todoister.adapter.RecyclerViewAdapter;
+import com.bawp.todoister.model.Priority;
+import com.bawp.todoister.model.Task;
+import com.bawp.todoister.model.TaskViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.View;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Calendar;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "ITEM";
+    private TaskViewModel taskViewModel;
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private int counter =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +38,29 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        recyclerView= findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        taskViewModel = new ViewModelProvider.AndroidViewModelFactory(
+                MainActivity.this.getApplication()).create(TaskViewModel.class);
+
+        taskViewModel.getAllTasks().observe(this, tasks -> {
+            recyclerViewAdapter = new RecyclerViewAdapter(tasks);
+            recyclerView.setAdapter(recyclerViewAdapter);
+        });
+
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+
+        fab.setOnClickListener(view -> {
+            Task task = new Task(
+                    "Task " + counter++,
+                    Priority.MEDIUM ,
+                    Calendar.getInstance().getTime(),
+                    Calendar.getInstance().getTime(),
+                    false);
+
+            TaskViewModel.insert(task);
         });
     }
 
--- app/src/main/java/com/bawp/todoister/util/TaskRoomDatabase.java
+++ app/src/main/java/com/bawp/todoister/util/TaskRoomDatabase.java
@@ -7,6 +7,7 @@ import androidx.room.Dao;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.bawp.todoister.data.TaskDao;
@@ -16,13 +17,15 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Database(entities = {Task.class}, version = 1, exportSchema = false)
+@TypeConverters({Converters.class})
+
 public abstract class TaskRoomDatabase extends RoomDatabase {
 
     public static final int NUMBER_OF_THREADS = 4;
     public static final String DATABASE_NAME = "todoister_database";
     private static volatile TaskRoomDatabase INSTANCE;
     public static final ExecutorService databaseWriterExecutor
-            
+
             = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
     public static final RoomDatabase.Callback sRoomDatabaseCallback
